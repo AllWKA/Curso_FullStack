5e91c62b2c006b11b2bafda1ee482442b8e53f3b
AllWKa <bryanjaramillobaldeon@gmail.com>
2019-08-07 21:18:45 +0100

    todo server done

---
+++ NodeJs/3_ex/server/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+
+var todo = {};
+// var ids = [];
+
+app.use(express.json());
+
+app.get('/list', function (req, res) {
+    res.send(todo)
+})
+
+app.post('/item', function (req, res) {
+    var id = newId();
+    var newItem = {
+        name: req.body.name,
+        status: false
+    }
+    todo[id] = newItem
+    res.send('Done:\n' + JSON.stringify(todo[id]));
+})
+
+app.put('/:id', function (req, res) {
+    if (Object.keys(todo).length - 1 >= req.params.id) {
+        todo[req.params.id].name = req.body.name;
+        todo[req.params.id].status = req.body.status;
+        return res.send(todo[req.params.id]);
+    } else {
+        return res.send("no existe wey")
+    }
+})
+app.put('/done/:id', function (req, res) {
+    if (Object.keys(todo).length - 1 >= req.params.id) {
+        todo[req.params.id].status = true;
+        return res.send(todo[req.params.id]);
+    } else {
+        return res.send("no existe wey")
+    }
+})
+app.put('/pending/:id', function (req, res) {
+    if (Object.keys(todo).length - 1 >= req.params.id) {
+        todo[req.params.id].status = false;
+        return res.send(todo[req.params.id]);
+    } else {
+        return res.send("no existe wey")
+    }
+})
+app.delete('/:id', function (req, res) {
+    delete todo[req.params.id]
+    newId()
+    res.send('Hello World')
+})
+
+function newId() {
+    var keys = []
+    var i = 0;
+    for (var k in todo) {
+        keys[i] = k
+        i++
+    };
+    if (keys[0] != "0") {
+        return 0;
+    } else if (keys[1] != "1") {
+        return 1;
+    } else {
+        for (let j = 0; j < keys.length - 1; j++) {
+            if (keys[j + 1] - keys[j] != 1) {
+                return Number.parseInt(keys[j]) + 1;
+            }
+        }
+        return Object.keys(todo).length;
+    }
+}
+app.listen(3000)
