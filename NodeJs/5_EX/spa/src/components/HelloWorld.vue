0d867a0abc3558e04dab3b2eb11cf7c650a7d164
AllWKa <bryanjaramillobaldeon@gmail.com>
2019-08-17 11:53:11 +0100

    mergin in pc

--- NodeJs/5_EX/spa/src/components/HelloWorld.vue
+++ NodeJs/5_EX/spa/src/components/HelloWorld.vue
@@ -9,10 +9,20 @@
         <input type="text" v-model="newContact.name" placeholder="Set name contact..." />
         <label>email</label>
         <input type="text" v-model="newContact.email" placeholder="Set email contact..." />
+        <button>create</button>
       </form>
     </div>
     <div>
-      <ul id="contactList"></ul>
+      <ul id="contactList">
+        <li v-for="(contact,i) in contacts" :key="i">
+          <div class="item">
+            <div>O</div>
+            <div>{{contact.name}}</div>
+            <div>{{contact.email}}</div>
+            <div>X</div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -22,7 +32,12 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      contats: [],
+      contacts: [
+        {
+          name: "z",
+          email: "x"
+        }
+      ],
       newContact: {
         name: "",
         email: ""
@@ -43,18 +58,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+li div {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 }
 </style>
