71c695beaedcc6841f37585f049bff3939912f59
AllWKa <bryanjaramillobaldeon@gmail.com>
2019-08-20 21:49:48 +0100

    Rebirth

--- NodeJs/MyMarvelList/my-marvel-list-app/src/keys.js
+++
@@ -1,7 +0,0 @@
-var privateKey = "05bb722fd581fd3af59ea4fd80c90657f42ae99d"
-var ts = "1"
-var publicKey = "12a72c197e619525e07ec4d79d4dbc58"
-var hash = "6b13a6e30093c088c16a00da05c18477"
-var url = "https://gateway.marvel.com:443/v1/public/"
-var fastVariable = "?apikey=12a72c197e619525e07ec4d79d4dbc58&ts=1&hash=6b13a6e30093c088c16a00da05c18477"
-module.exports = { publicKey, privateKey, ts, hash, url, fastVariable }
