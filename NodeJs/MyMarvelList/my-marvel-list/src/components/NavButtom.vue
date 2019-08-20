80fbaacfd0691f4dfcb1c8e58cc7093155c2ea70
AllWKa <bryanjaramillobaldeon@gmail.com>
2019-08-20 13:50:31 +0100

    MyMarvelList Rebirth

---
+++ NodeJs/MyMarvelList/my-marvel-list/src/components/NavButtom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-buttom">
+    <h2>{{text}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: NavButtom,
+  props: {
+    text: String
+  },
+  data() {
+    return {};
+  }
+};
+</script>
